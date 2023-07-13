f76d652be0991f2e77f2a38f59d03d0548c22437
profleeys <leeys@mail.mcu.edu.tw>
2023-07-13 21:05:47 +0800

    Modify Main Program

--- Table_Extraction_S.py
+++ Table_Extraction_S.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from bs4 import BeautifulSoup
+import numpy as np
 import pandas as pd
 import base64
+import re
 
 def create_download_link(data, filename):
     b64 = base64.b64encode(data).decode()  # 將檔案數據轉換為 base64 編碼
@@ -32,16 +34,47 @@ if __name__ == '__main__':
         # 建立一個空的DataFrame
         combined_df = pd.DataFrame()
 
+        extracted_city = 'Sheet1'
+
         # 逐個處理每個表格
         for table in tables:
             # 將表格轉換為DataFrame
             df = pd.read_html(str(table))[0]
             
-            # 將DataFrame連接到combined_df
-            combined_df = pd.concat([combined_df, df])
+            if df.shape[0] < 3:
+                pattern = r'縣市別：(.*?) 製表日期'
+                result = re.search(pattern, df.iloc[0][1][:100])
+                if result:
+                    extracted_city = result.group(1).replace(' ', '')
+            else:
+                # 檢查每個 row 中的每個欄位是否至少有一個數字
+                #has_numeric_values = df.apply(lambda row: any(str(val).isdigit() or str(val) == '合 計' or str(val) == '申報大於歸戶' for val in row if pd.notnull(val)), axis=1)
+                has_numeric_values = df.apply(lambda row: any(str(val).isdigit() for val in row if pd.notnull(val)), axis=1)
+
+                # 保留至少有一個欄位的非空值為數字的資料
+                df = df[has_numeric_values]
+                
+                # 將DataFrame連接到combined_df
+                combined_df = pd.concat([combined_df, df])
+
+        #combined_df = combined_df.drop(combined_df.index[0])  # 刪除第一個行
+        combined_df = combined_df.drop(combined_df.columns[0], axis=1).reset_index(drop=True)  # 刪除第一個列
+
+        combined_df.iloc[:, 0] = combined_df.iloc[:, 0].fillna(method='ffill', axis=0)
+        
+        split_index = combined_df.shape[0] // 2
+        table1 = combined_df.iloc[:split_index, :-1]
+        table2 = combined_df.iloc[split_index:, :-3]
+        
+        table1.columns = ['區鄉','里村','納稅單位','合計','營利所得','執行業務所得','薪資所得','利息所得','租賃及權利金','財產交易所得','機會中獎所得']
+        table2.columns = ['區鄉','里村','股利所得','退職所得','其他所得','稿費所得','申報大於歸戶','薪資收入','稿費收入']
+
+        # Perform a full outer join on columns 'A' and 'B'
+        combined_df = pd.merge(table1, table2, on=['區鄉','里村'], how='outer')
 
         # 將結果寫入Excel檔案
-        combined_df.to_excel('converted.xlsx', index=False)
+        combined_df.to_excel('converted.xlsx', index=False, header=True, sheet_name=extracted_city)
+        combined_df.to_csv('converted.csv', index=False, header=True)
         
         #with open('converted.xlsx', 'rb') as f:
         #    data = f.read()
